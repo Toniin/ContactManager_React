3a42a1d88a348ddce55ac635e37bf561ed6e2f25
Antonin <antonin.chaudiere@hotmail.fr>
2024-08-06 11:47:04 +0200

    Rearrange redux actions

--- src/redux/actions/contact.action.ts
+++ src/redux/actions/contact.action.ts
@@ -2,31 +2,41 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {Contact} from "@/models/contact.model.ts";
 import {contactsAPI} from "@/lib/axios.ts";
 
-export const addContact = createAsyncThunk('addContact', async (newContact: Contact) => {
-    return await contactsAPI.post("/add", JSON.stringify(newContact))
-        .then(response => response.data)
-        .catch(error => error.response.data);
-})
+export const addContact = createAsyncThunk(
+    'addContact',
+    (newContact: Contact) => {
+        return contactsAPI.post("/add", JSON.stringify(newContact))
+            .then(response => response.data)
+            .catch(error => error.response.data);
+    })
 
-export const getContacts = createAsyncThunk('getContacts', async () => {
-    const response = await contactsAPI.get("")
-    return response.data
-})
+export const getContacts = createAsyncThunk(
+    'getContacts',
+    () => {
+        return contactsAPI.get("")
+            .then(response => response.data)
+    })
 
-export const getContact = createAsyncThunk('getContact', async (phoneNumber: number) => {
-    return contactsAPI.get(`/find/${phoneNumber}`)
-        .then(response => response.data)
-        .catch(error => error.response.data);
-})
+export const getContact = createAsyncThunk(
+    'getContact',
+    (phoneNumber: number) => {
+        return contactsAPI.get(`/find/${phoneNumber}`)
+            .then(response => response.data)
+            .catch(error => error.response.data);
+    })
 
-export const deleteContact = createAsyncThunk('deleteContact', async (phoneNumber: number) => {
-    return await contactsAPI.delete(`/delete/${phoneNumber}`)
-        .then(response => response.data)
-        .catch(error => error.response.data)
-})
+export const deleteContact = createAsyncThunk(
+    'deleteContact',
+    (phoneNumber: number) => {
+        return contactsAPI.delete(`/delete/${phoneNumber}`)
+            .then(response => response.data)
+            .catch(error => error.response.data)
+    })
 
-export const renameContact = createAsyncThunk('renameContact', async (renamedContact: Contact) => {
-    return contactsAPI.put(`/update/${renamedContact.phoneNumber}`, JSON.stringify(renamedContact))
-        .then(response => response.data)
-        .catch(error => error.response.data);
-})
+export const renameContact = createAsyncThunk(
+    'renameContact',
+    (renamedContact: Contact) => {
+        return contactsAPI.put(`/update/${renamedContact.phoneNumber}`, JSON.stringify(renamedContact))
+            .then(response => response.data)
+            .catch(error => error.response.data);
+    })
--- src/redux/actions/user.action.ts
+++ src/redux/actions/user.action.ts
@@ -2,14 +2,18 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {SignInFormUser, SignUpFormUser} from "@/models/user.model.ts";
 import {contactsAPI} from "@/lib/axios.ts";
 
-export const signUp = createAsyncThunk('signUp', async (newUser: SignUpFormUser) => {
-    return await contactsAPI.post("/auth/register", JSON.stringify(newUser))
-        .then(response => response.data)
-        .catch(error => error.response.data);
-})
+export const signUp = createAsyncThunk(
+    'signUp',
+    (newUser: SignUpFormUser) => {
+        return contactsAPI.post("/auth/register", JSON.stringify(newUser))
+            .then(response => response.data)
+            .catch(error => error.response.data);
+    })
 
-export const signIn = createAsyncThunk('signIn', async (user: SignInFormUser) => {
-    return await contactsAPI.post("/auth/login", JSON.stringify(user))
-        .then(response => response.data)
-        .catch(error => error.response.data);
-})
+export const signIn = createAsyncThunk(
+    'signIn',
+    (user: SignInFormUser) => {
+        return contactsAPI.post("/auth/login", JSON.stringify(user))
+            .then(response => response.data)
+            .catch(error => error.response.data);
+    })
