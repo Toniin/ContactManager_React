f16321d1aae351fcc27b0f079594044b4fe0e486
Antonin <antonin.chaudiere@hotmail.fr>
2024-08-06 10:34:06 +0200

    Fix type of newUser in signUp action

--- src/redux/actions/user.action.ts
+++ src/redux/actions/user.action.ts
@@ -1,8 +1,8 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import {SignInFormUser, User} from "@/models/user.model.ts";
+import {SignInFormUser, SignUpFormUser} from "@/models/user.model.ts";
 import {contactsAPI} from "@/lib/axios.ts";
 
-export const signUp = createAsyncThunk('signUp', async (newUser: User) => {
+export const signUp = createAsyncThunk('signUp', async (newUser: SignUpFormUser) => {
     return await contactsAPI.post("/auth/register", JSON.stringify(newUser))
         .then(response => response.data)
         .catch(error => error.response.data);
