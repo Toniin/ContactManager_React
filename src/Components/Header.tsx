5816ce644b8bf38f41854ccd81f8bc29257014e1
Antonin <antonin.chaudiere@hotmail.fr>
2024-08-06 12:58:49 +0200

    Add height: 100vh on body

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     const user = useAppSelector(state => state.persistedReducer)
 
     return (
-        <header className="container flex justify-between items-center my-10">
+        <header className="container flex justify-between items-center py-10">
             <h1 className="text-5xl">Contact manager</h1>
             {user.isSignIn ?
                 <ButtonSignOut/>
