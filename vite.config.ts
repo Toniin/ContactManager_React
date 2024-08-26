c817380e2425bde7889c1db257c3fa89da6d5a94
Antonin CH <43450497+Toniin@users.noreply.github.com>
2024-08-26 11:40:01 +0200

    Update vite.config.ts
    Vite config : Add preserveSymlinks to true

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // preserveSymlinks: true,
+    preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
